8b634dc47d6a13d8b84e09e8a78a94259b837c85
Jose Cortes <josemcortesh@gmail.com>
2023-12-03 17:28:53 -0500

    initial commit

---
+++ flask-app/app.py
@@ -0,0 +1,25 @@
+from flask import Flask
+import time
+import redis
+
+app = Flask(__name__)
+cache = redis.Redis(host="172.20.0.3", port=6379)
+
+def get_hit_count():
+    retries = 5
+    while True:
+        try:
+            return cache.incr('hits')
+        except redis.exceptions.ConnectionError as exc:
+            if retries == 0:
+                raise exc
+            retries -= 1
+            time.sleep(0.5)
+
+@app.route('/')
+def index():
+    count = get_hit_count()
+    return 'Hola Mundo! I have been seen {} times.\n' .format(count)
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=5000)
